338d6d14322bee308ba66281e612c3d5d1f84b81
MaxRobertsDear <maxhroberts@gmail.com>
2019-05-19 10:51:47 +0100

    test that input is accepted

--- cypress/integration/input-form.Spec.js
+++ cypress/integration/input-form.Spec.js
@@ -1,5 +1,16 @@
 describe(`Input form`, () => {
   it(`focuses input on load`, () => {
     cy.visit(`http://localhost:3030`)
+
+    cy.focused()
+      .should(`have.class`, `new-todo`)
+  })
+  it.only(`accepts input`, () => {
+    let typedText = `Buy milk`
+    cy.visit(`http://localhost:3030`)
+
+    cy.get(`.new-todo`)
+      .type(typedText)
+      .should(`have.value`, typedText)
   })
 })
