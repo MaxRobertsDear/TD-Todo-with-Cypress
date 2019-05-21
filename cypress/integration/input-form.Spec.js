038dfd0f26aa279f6f37835dfaf88113b8145e67
MaxRobertsDear <maxhroberts@gmail.com>
2019-05-21 22:03:06 +0100

    return error when api call unsuccessful

--- cypress/integration/input-form.Spec.js
+++ cypress/integration/input-form.Spec.js
@@ -18,7 +18,7 @@ describe(`Input form`, () => {
   })
 
   context(`Form submission`, () => {
-    it.only(`adds a new todo on submit`, () => {
+    it(`adds a new todo on submit`, () => {
       const newItem = 'buy eggs'
       cy.server()
       cy.route('POST', '/api/todos', {
@@ -33,6 +33,24 @@ describe(`Input form`, () => {
       cy.get('.todo-list li')
         .should('have.length', 1)
         .and('contain', newItem)
+    })
+
+    it.only('shows error message on failed submissoin', () => {
+      cy.server()
+      cy.route({
+        url: '/api/todos',
+        method: 'POST', 
+        status: 500, 
+        response: {} 
+      })
+      cy.get('.new-todo')
+        .type('test{enter}')
+
+      cy.get('.todo-list li')
+        .should('not.exist')
+
+      cy.get('.error')
+        .should('be.visible')
     })
   })
 })
